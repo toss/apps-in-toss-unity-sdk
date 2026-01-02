906e0e0ee8fd80e4aff3c4f0c2ea9e6ea9506976
Elik Song <elik.song@icloud.com>
2026-01-02 23:26:05 +0900

    Edit authentication await task method Task to Awaitable

--- Runtime/SDK/AIT.Authentication.cs
+++ Runtime/SDK/AIT.Authentication.cs
@@ -9,6 +9,9 @@ using System;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine.Scripting;
+#if UNITY_6000_0_OR_NEWER
+using UnityEngine;
+#endif
 
 namespace AppsInToss
 {
@@ -20,7 +23,11 @@ namespace AppsInToss
         /// <exception cref="AITException">Thrown when the API call fails</exception>
         [Preserve]
         [APICategory("Authentication")]
+#if UNITY_6000_0_OR_NEWER
+        public static async Awaitable<AppLoginResult> AppLoginAsync()
+#else
         public static async Task<AppLoginResult> AppLogin()
+#endif
         {
 #if UNITY_WEBGL && !UNITY_EDITOR
             var tcs = new TaskCompletionSource<AppLoginResult>();
@@ -46,7 +53,11 @@ namespace AppsInToss
         /// <exception cref="AITException">Thrown when the API call fails</exception>
         [Preserve]
         [APICategory("Authentication")]
+#if UNITY_6000_0_OR_NEWER
+        public static async Awaitable<bool?> GetIsTossLoginIntegratedServiceAsync()
+#else
         public static async Task<bool?> GetIsTossLoginIntegratedService()
+#endif
         {
 #if UNITY_WEBGL && !UNITY_EDITOR
             var tcs = new TaskCompletionSource<bool?>();

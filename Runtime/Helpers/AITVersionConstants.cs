e89a3874439f7c8ccbd59381cc7bb96626eb9b3c
Dave Choi <dave.dev@icloud.com>
2026-01-26 21:04:28 +0900

    수정: release.yml heredoc 들여쓰기 문제 해결
    - AITVersionConstants.cs 생성 시 들여쓰기가 파일에 포함되는 문제 수정
    - 플레이스홀더 방식으로 변경하여 정확한 포맷 보장

--- Runtime/Helpers/AITVersionConstants.cs
+++ Runtime/Helpers/AITVersionConstants.cs
@@ -1,10 +1,10 @@
 // Auto-generated - DO NOT EDIT MANUALLY
-// Updated at: 2026-01-26T11:59:38Z
+// Updated at: 2026-01-26T12:04:28Z
 namespace AppsInToss
 {
     internal static class AITVersionConstants
     {
         public const string Version = "1.8.0";
-        public const string ReleaseDateTime = "20260126_1159";
+        public const string ReleaseDateTime = "20260126_1204";
     }
 }

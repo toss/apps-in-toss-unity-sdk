c899f6915d82b49348a76419a312faada44f00b9
최병훈B <dave.v2@toss.im>
2026-04-14 11:54:28 +0900

    문서: 전체 리뷰 기반 TODO.md 추가
    문서 정합성 이슈 6건, 코드 구조 개선 5건, 비동기/스레딩 개선 3건,
    에러 처리 개선 1건, 의존성 관리 2건, 코드 중복 제거 3건 등
    우선순위별 개선 항목을 정리한 TODO.md를 추가한다.

--- Runtime/Helpers/AITVersionConstants.cs
+++ Runtime/Helpers/AITVersionConstants.cs
@@ -1,11 +1,11 @@
 // Auto-generated - DO NOT EDIT MANUALLY
-// Updated at: 2026-04-14T02:46:52Z
+// Updated at: 2026-04-14T02:54:28Z
 namespace AppsInToss
 {
     internal static class AITVersionConstants
     {
         public const string Version = "2.4.6";
-        public const string ReleaseDateTime = "20260414_0246";
+        public const string ReleaseDateTime = "20260414_0254";
         public const string CommitHash = "3e70dfe";
     }
 }

bf2f37b17030a77c2b71fed58b7ad824bfcafb6e
최병훈B <dave.v2@toss.im>
2026-04-16 15:19:13 +0900

    개선: TryResolveDirectory/File 편의 메서드 추가, 경고에 시도 경로 포함, 문서 보강

--- Runtime/Helpers/AITVersionConstants.cs
+++ Runtime/Helpers/AITVersionConstants.cs
@@ -1,11 +1,11 @@
 // Auto-generated - DO NOT EDIT MANUALLY
-// Updated at: 2026-04-16T04:48:09Z
+// Updated at: 2026-04-16T06:19:13Z
 namespace AppsInToss
 {
     internal static class AITVersionConstants
     {
         public const string Version = "2.4.6";
-        public const string ReleaseDateTime = "20260416_0448";
-        public const string CommitHash = "72f322a";
+        public const string ReleaseDateTime = "20260416_0619";
+        public const string CommitHash = "43ed15d";
     }
 }

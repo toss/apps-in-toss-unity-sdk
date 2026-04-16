5d0c4ce22851e1ebad169446ffd6f79bc5ab2bf7
최병훈B <dave.v2@toss.im>
2026-04-16 15:27:39 +0900

    개선: assemblyAnchor 파라미터에 asmdef 일치 요구사항 문서화

--- Runtime/Helpers/AITVersionConstants.cs
+++ Runtime/Helpers/AITVersionConstants.cs
@@ -1,11 +1,11 @@
 // Auto-generated - DO NOT EDIT MANUALLY
-// Updated at: 2026-04-16T06:25:30Z
+// Updated at: 2026-04-16T06:27:39Z
 namespace AppsInToss
 {
     internal static class AITVersionConstants
     {
         public const string Version = "2.4.6";
-        public const string ReleaseDateTime = "20260416_0625";
-        public const string CommitHash = "77723c2";
+        public const string ReleaseDateTime = "20260416_0627";
+        public const string CommitHash = "bd58fd3";
     }
 }

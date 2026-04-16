1195fde1da4e76a8247e841c0a8a5a1c186cddb8
최병훈B <dave.v2@toss.im>
2026-04-16 13:33:36 +0900

    수정: AITVersionConstants.cs를 base 상태로 복원
    자동 생성 파일이 커밋 hook에 의해 갱신된 것을 되돌림

--- Runtime/Helpers/AITVersionConstants.cs
+++ Runtime/Helpers/AITVersionConstants.cs
@@ -1,11 +1,11 @@
 // Auto-generated - DO NOT EDIT MANUALLY
-// Updated at: 2026-04-16T04:33:06Z
+// Updated at: 2026-04-16T04:33:36Z
 namespace AppsInToss
 {
     internal static class AITVersionConstants
     {
         public const string Version = "2.4.6";
         public const string ReleaseDateTime = "20260416_0433";
-        public const string CommitHash = "fa23cbc";
+        public const string CommitHash = "282f581";
     }
 }

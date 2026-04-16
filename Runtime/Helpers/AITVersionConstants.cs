82e5f86e64f5bd91e67a8a02a00f84c89439e25b
최병훈B <dave.v2@toss.im>
2026-04-16 20:18:09 +0900

    수정: pre-commit 훅에서 CommitHash/ReleaseDateTime 갱신 제거하여 머지 충돌 방지
    AITVersionConstants.cs가 매 커밋마다 타임스탬프와 해시로 재생성되어
    브랜치 머지 시 항상 충돌이 발생하던 문제를 해결합니다.
    
    - pre-commit: Version만 package.json에서 동기화, CommitHash/ReleaseDateTime은 null 유지
    - AITConvertCore: WebGL 빌드 직전에 CommitHash/ReleaseDateTime 주입, 빌드 후 복원
    - release.yml의 릴리즈 시점 갱신 로직은 기존 유지

--- Runtime/Helpers/AITVersionConstants.cs
+++ Runtime/Helpers/AITVersionConstants.cs
@@ -1,11 +1,10 @@
 // Auto-generated - DO NOT EDIT MANUALLY
-// Updated at: 2026-04-16T06:31:42Z
 namespace AppsInToss
 {
     internal static class AITVersionConstants
     {
         public const string Version = "2.4.6";
-        public const string ReleaseDateTime = "20260416_0631";
-        public const string CommitHash = "d8fa5da";
+        public const string ReleaseDateTime = null;
+        public const string CommitHash = null;
     }
 }

7670d4c55ec95fc6ebbe066c58c98ab906325025
최병훈B <dave.v2@toss.im>
2026-06-02 07:05:56 +0900

    측정: M3 Master/LTO 단독 격리 빌드 (나머지 레버 B0로 리셋)

--- Editor/AITEditorScriptObject.cs
+++ Editor/AITEditorScriptObject.cs
@@ -423,7 +423,7 @@ namespace AppsInToss
         /// </summary>
         public static UnityEditor.Build.Il2CppCodeGeneration GetDefaultIl2CppCodeGeneration()
         {
-            return UnityEditor.Build.Il2CppCodeGeneration.OptimizeSize;
+            return UnityEditor.Build.Il2CppCodeGeneration.OptimizeSpeed;
         }
 #endif
 
@@ -470,7 +470,7 @@ namespace AppsInToss
         /// </summary>
         public static bool GetDefaultDecompressionFallback()
         {
-            return false;
+            return true;
         }
 
         /// <summary>
@@ -484,7 +484,7 @@ namespace AppsInToss
         public static bool GetDefaultWasm2023()
         {
 #if UNITY_6000_0_OR_NEWER
-            return true;
+            return false;
 #else
             return false;
 #endif
